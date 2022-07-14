b3bd61aa39040ca6d2a59db3c659a8f79ba6bb2d
Sachin Iyer <sachinjiyer@gmail.com>
2022-07-14 16:30:22 -0700

    add some ein stuff and remove unecessary comments

--- config.el
+++ config.el
@@ -1,82 +1,19 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
+;; ME
 (setq user-full-name "Sachin Iyer"
       user-mail-address "sachinjiyer@gmail.com")
 
 
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
+;; THEMING
 (setq doom-font (font-spec :family "Hack" :size 12 :weight 'normal)
       doom-variable-pitch-font (font-spec :family "Hack" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-zenburn)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
+(setq doom-modeline-height 18)
+(setq doom-modeline-project-detection 'auto)
+(setq doom-modeline-minor-modes nil)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
 
-
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
+;; UTILITY FUNCTIONS
 (defun ds/counsel-linux-app-format-function (name comment exec)
   (format "% -45s %s"
           (propertize name 'face 'font-lock-builtin-face)
@@ -140,6 +77,7 @@
       "s-f" #'find-file)
 
 
+;; GENERAL CONFIG
 
 ;; EXWM
 (use-package! exwm
@@ -197,6 +135,12 @@
 (use-package! org
   :config
   (add-hook! 'org-mode-hook #'auto-fill-mode))
+(setq org-directory "~/docs/todo/")
+
+;; PYTHON
+(use-package! ein
+  :config
+  (setq ein:output-area-inlined-images t))
 
 
 ;; PDF
