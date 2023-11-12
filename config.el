6f1fb854b43dc8325f048ee1f1d3ccc1489f397d
Sachin Iyer <sachinjiyer@gmail.com>
2023-11-11 16:41:46 -0800

    automatically theme pdfs

--- config.el
+++ config.el
@@ -379,29 +379,8 @@
   (mmm-add-mode-ext-class 'rustic-mode nil 'mmm-web-rust))
 
 ;; PDF
-(after! pdf-view
-  ;; open pdfs scaled to fit page
-  (add-hook! 'pdf-view-mode-hook #'pdf-view-themed-minor-mode)
-  (setq-default pdf-view-display-size #'fit-width)
-  (add-hook! 'pdf-view-mode-hook (evil-colemak-basics-mode -1))
-  ;; automatically annotate highlights
-  (setq pdf-annot-activate-created-annotations t
-        pdf-view-resize-factor 1.1)
-  ;; faster motion
-  (map!
-   :map pdf-view-mode-map
-   :n "g g"          #'pdf-view-first-page
-   :n "G"            #'pdf-view-last-page
-   :n "N"            #'pdf-view-next-page-command
-   :n "E"            #'pdf-view-previous-page-command
-   :n "e"            #'evil-collection-pdf-view-previous-line-or-previous-page
-   :n "n"            #'evil-collection-pdf-view-next-line-or-next-page
-   :localleader
-   (:prefix "o"
-            (:prefix "n"
-             :desc "Insert" "i" 'org-noter-insert-note
-             ))
-   ))
+(use-package! pdf-view
+  :hook (pdf-view-mode . pdf-view-themed-minor-mode))
 
 ;; PROJECTILE
 (after! projectile (setq projectile-project-root-files-bottom-up (remove ".git"
